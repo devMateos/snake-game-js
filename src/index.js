3ba4add0c74e5af99c4e62a3650821918f169a7b
Antonio Mateos <antoniomateosmariscal@gmail.com>
2022-06-20 21:09:05 +0200

    Adding some comments in js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const STATE_RUNNING = 1; // Juego corriendo
 const STATE_LOSING = 2; // Perdiendo
 
-const TICK = 80; // Intervalo de tiempo entre los desplazamientos del gusano (en milisegundos)
+const SPEED = 100; // Intervalo de tiempo entre los desplazamientos del gusano (en milisegundos)
 const SQUARE_SIZE = 10; // Tamaño de los cuadros que se van a dibujar sobre el área de juego
 
 // Ancho y alto virtual de nuestro espacio de juego
 const BOARD_WIDTH = 60;
 const BOARD_HEIGHT = 60;
-const GROW_SCALE = 1; // Cuánto crecerá el gusano
-const DIRECTIONS_MAP = { // Cambia la dirección hacia la que se mueve el gusano
+
+const GROW_SCALE = 1; // Cuánto crecerá el gusano cada vez que coma
+const DIRECTIONS_MAP = { // Cambia la dirección hacia la que se mueve el gusano. El primer elemento es el desplazamiento que hará en el eje X y el segundo el desplazamiento que hará en el eje Y
     A: [-1, 0],
     D: [1, 0],
     S: [0, 1],
@@ -25,11 +26,12 @@ let state = {
     context: null,
     worm: [{ x: 0, y: 0 }],
     direction: { x: 1, y: 0 },
-    prey: { x: 0, y: 0 },
+    food: { x: 0, y: 0 },
     growing: 0,
     runState: STATE_RUNNING
 };
 
+//función para generar posiciones aleatorias
 function randomXY() {
     return {
         x: parseInt(Math.random() * BOARD_WIDTH),
@@ -37,28 +39,29 @@ function randomXY() {
     };
 }
 
+//
 function tick() {
     const head = state.worm[0];
     const dx = state.direction.x;
     const dy = state.direction.y;
     const highestIndex = state.worm.length - 1;
     let tail = {};
-    let interval = TICK;
+    let interval = SPEED;
 
     Object.assign(tail, state.worm[state.worm.length - 1]);
 
-    let didScore = (head.x === state.prey.x && head.y === state.prey.y);
+    let didScore = (head.x === state.food.x && head.y === state.food.y);
 
     if (state.runState === STATE_RUNNING) {
-        for (let idx = highestIndex; idx > -1; idx--) {
-            const sq = state.worm[idx];
+        for (let i = highestIndex; i > -1; i--) {
+            const sq = state.worm[i];
 
-            if (idx === 0) {
+            if (i === 0) {
                 sq.x += dx;
                 sq.y += dy;
             } else {
-                sq.x = state.worm[idx - 1].x;
-                sq.y = state.worm[idx - 1].y;
+                sq.x = state.worm[i - 1].x;
+                sq.y = state.worm[i - 1].y;
             }
         }
     } else if (state.runState === STATE_LOSING) {
@@ -71,7 +74,7 @@ function tick() {
         if (state.worm.length === 0) {
             state.runState = STATE_RUNNING;
             state.worm.push(randomXY());
-            state.prey = randomXY();
+            state.food = randomXY();
         }
     }
 
@@ -82,7 +85,7 @@ function tick() {
 
     if (didScore) {
         state.growing += GROW_SCALE;
-        state.prey = randomXY();
+        state.food = randomXY();
     }
 
     if (state.growing > 0) {
@@ -116,7 +119,7 @@ function detectCollision() {
     return false;
 }
 
-// Función que dibuja cada uno de los cuadritos que componen el gusano
+// Función que dibuja cada uno de los cuadritos que componen el gusano o la comida
 function drawPixel(color, x, y) {
     state.context.fillStyle = color;
     state.context.fillRect(
@@ -126,28 +129,30 @@ function drawPixel(color, x, y) {
         SQUARE_SIZE
     );
 }
-
+// Función principal de dibujado
 function draw() {
-    state.context.clearRect(0, 0, 600, 600);
-
-    for (var idx = 0; idx < state.worm.length; idx++) {
-        const { x, y } = state.worm[idx];
+    state.context.clearRect(0, 0, 600, 600);    //Borramos el contexto
+    // Recorrido por cada uno de los puntos que abarca la variable worm
+    for (var i = 0; i < state.worm.length; i++) {
+        const { x, y } = state.worm[i];
         drawPixel("#F79F71", x, y);
     }
-
-    const { x, y } = state.prey;
+    // Función para dibujar la comida
+    const { x, y } = state.food;
     drawPixel("yellow", x, y);
 }
 
+//Inicializando los valores necesarios
 window.onload = function () {
-    state.canvas = document.querySelector("canvas");
-    state.context = state.canvas.getContext("2d");
-
+    state.canvas = document.querySelector("canvas");    //Inicialización del evento canvas
+    state.context = state.canvas.getContext("2d");      //Inicialización del contexto de dibujo
+    //Mecanismo de presión de teclas
     window.onkeydown = function (e) {
         const direction = DIRECTIONS_MAP[e.key];
-
+        //Validando si la dirección de la tecla existe de acuerdo a la tecla
         if (direction) {
             const [x, y] = direction;
+            //Comprobación de que la tecla pulsada no es la dirección a la que el gusano se está desplazando actualmente
             if (-x !== state.direction.x && -y !== state.direction.y) {
                 state.direction.x = x;
                 state.direction.y = y;
